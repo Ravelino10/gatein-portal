6513280f7182e518553b512852a760c595dc562a
Ravelino10 <ruan_avelino@hotmail.com>
2015-08-20 09:33:16 -0300

    Lanche.java

---
+++ Lanche.java
@@ -0,0 +1,19 @@
+abstract class Lanche {
+
+	String Ingrediente1, Ingrediente2, Ingrediente3, Ingrediente4;
+	double preco1, preco2, preco3, preco4;
+	
+	Lanche(String Ingrediente1, double preco1, String Ingrediente2, double preco2, String Ingrediente3, double preco3, String Ingrediente4, double preco4){
+		this.Ingrediente1 = Ingrediente1;
+		this.preco1 = preco1;
+		this.Ingrediente2 = Ingrediente2;
+		this.preco2 = preco2;
+		this.Ingrediente3 = Ingrediente3;
+		this.preco3 = preco3;
+		this.Ingrediente4 = Ingrediente4;
+		this.preco4 = preco4;
+	}	
+	
+	Carne 
+	
+}
